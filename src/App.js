44686cf1f1d197b1231f1955ed5a85047239f03a
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2023-01-15 19:48:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Services from "./components/Services/Services";
+import Skills from "./components/Skills/Skills";
+import Stats from "./components/Stats/Stats";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Work from "./components/Work/Work";
+import WorkProcess from "./components/WorkProcess/WorkProcess";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="holder">
+      <Header />
+      <AboutUs />
+      <Skills />
+      <Stats />
+      <Work />
+      <WorkProcess />
+      <Services />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer section__padding bg__dark">
+      <div className="container">
+        <div className="footer__content grid text__light text__center">
+          <div className="footer__content--item">
+            <p className="para__text">
+              &copy; 2023 All rights reserved. Designed by Mahmoud Nabil.
+            </p>
+          </div>
+
+          <div className="footer__content--item">
+            <a href="mailto:e.mahmoudnabil@gmail.com">
+              e.mahmoudnabil@gmail.com
+            </a>
+            <br />
+            <span>+201146285867</span>
+          </div>
+
+          <div className="footer__content--item">
+            <h3 className="footer__title">Projects</h3>
+            <ul className="footer__links">
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Career</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__content--item">
+            <h3 className="footer__title">News</h3>
+            <ul className="footer__links">
+              <li>
+                <a href="#">Events</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Legals</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__content--item">
+            <h3 className="footer__title">Social LInks</h3>
+            <ul className="footer__links">
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="footer__content--item" id="back-top">
+            <p class="pull-right">
+              <a href="#home">
+                <span></span> Top
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
